af20d3f8af813dba3efa3caca4aa3f60ff88d0c0
anas <anas.aljuhani.dev@gmail.com>
2024-07-31 23:49:15 +0300

    second function creation

--- dartfunction.dart
+++ dartfunction.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  printinfo();
+}
+
+printinfo() {
+  print(
+      'GitHub is a code hosting platform for version control and collaboration.');
+  print(
+      'It allows developers to store, manage, and track changes to their code.');
+  print(
+      'GitHub also offers features like issue tracking, code review, and project management.');
+  print(
+      'It is widely used by individuals and organizations for open source and private projects.');
+}
+
+printDartInfo() {
+  print(
+      'Dart is a client-optimized language for developing fast apps on any platform.');
+  print(
+      'It\'s known for its strong type system, null safety, and hot reload feature.');
+  print('Flutter, a popular UI toolkit, is built with Dart.');
+}
