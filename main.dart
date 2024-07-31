63046b2d9f65f07724f6a37c4353e3e0028bd079
Linda Mak <LindaAlmakramy@gmail.com>
2024-07-31 14:39:00 +0300

    added main and function for q1

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'question_one.dart';
+
+void function(){
+  print("\n\n\n\n\n");
+  print("< ---- Welcome to Git quiz ---->");
+  print("Lets 'git' started, enter 1 to go to question 1");
+  String? goToQ1 = stdin.readLineSync();
+      while(goToQ1!='1'){
+        print("Please try again");
+        goToQ1 = stdin.readLineSync();
+      }
+      question1();
+}
