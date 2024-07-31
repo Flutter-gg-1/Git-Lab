3810caf57b11bcc6354cd3a7ad4e0e393c635458
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-07-31 14:49:58 +0300

    create a function to print a colorized text in console

---
+++ bin/functions/menu_display.dart
@@ -0,0 +1,19 @@
+import '../utility/print_with_color.dart';
+
+/*
+*
+* This function will display menu options
+*
+* */
+void displayMenu() {
+  // Print menu options
+  printWithColor(text: 'CLI Calculator program', color: 'Yellow');
+  printWithColor(text: '1.Addition', color: 'Blue');
+  printWithColor(text: '2.Subtraction', color: 'Blue');
+  printWithColor(text: '3.Multiplication', color: 'Blue');
+  printWithColor(text: '4.Division', color: 'Blue');
+  printWithColor(text: '5.Modulus', color: 'Blue');
+
+  // Print with yellow color
+  printWithColor(text: '7. Exit');
+}
--- bin/git_lab_1.dart
+++ bin/git_lab_1.dart
@@ -1,5 +1,41 @@
-import 'package:git_lab_1/git_lab_1.dart' as git_lab_1;
+import './functions/menu_display.dart';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${git_lab_1.calculate()}!');
+// Here we go :)
+void main() {
+
+  do{
+    // To show my menu options
+    displayMenu();
+
+    // Write a message in CLI
+    stdout.write('Choose an option: ');
+
+    // Get user input
+    final input = stdin.readLineSync();
+
+    // Switch case to handle user input
+    switch (input) {
+      case '1':
+        print('Addition');
+        break;
+      case '2':
+        print('Subtraction');
+        break;
+      case '3':
+        print('Multiplication');
+        break;
+      case '4':
+        print('Division');
+        break;
+      case '5':
+        print('Modulus');
+        break;
+      case '6':
+        print('Exit');
+        break;
+      default:
+        print('Invalid option');
+    }
+  }while(true);
 }
