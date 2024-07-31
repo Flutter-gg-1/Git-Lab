79b60c8ebb6b0264617028f43cefa0132f39374f
Rahafahg <irahaf.88@gmail.com>
2024-07-31 16:22:46 +0300

    Rahaf Alghamdi

--- quiz_app/bin/quiz_app.dart
+++ quiz_app/bin/quiz_app.dart
@@ -7,14 +7,19 @@ void main(List<String> arguments) {
   print("b. commited");
   print("c. staged");
 
-  String answer = stdin.readLineSync()!;
-  print(quiz(answer: answer));
+  String answer1 = stdin.readLineSync()!;
+  print(quiz(answer: answer1));
+
+  print("\n \n -------- add commands you know in git:");
+  String answer2 = stdin.readLineSync()!;
+  print(quiz(answer: answer2));
 }
 
+//function 1
 quiz({required String answer}) {
   switch (answer) {
     case "a":
-      print("\n coorect answer");
+      print("\n correct answer");
       break;
     case "b":
       print("\n wrong answer");
@@ -26,4 +31,14 @@ quiz({required String answer}) {
   }
 
   return answer;
+}
+
+//function
+
+addGitCommands({required String commands}) {
+  List gitcommands = [];
+
+  gitcommands.add(commands);
+
+  return gitcommands;
 }
