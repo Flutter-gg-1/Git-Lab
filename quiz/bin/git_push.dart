5de04241a7e4e4875e30c5e9918a3a157dbf167e
yara albouq <yaramalbouq@gmail.com>
2024-07-31 17:47:17 +0300

    modify push function

--- quiz/bin/git_push.dart
+++ quiz/bin/git_push.dart
@@ -1,5 +1,14 @@
 import 'dart:io';
 gitPush() {
-  
-  
+  print("0 for true, 1 for false");
+  print(
+      "git push is use to Updates remote refs using local refs, while sending objects necessary to complete the given refs?");
+      print("Enter your choice:");
+  String? choice = stdin.readLineSync();
+  if (choice == "0") {
+    print("true");
+  } else {
+    print("false");
+  }
+
 }
