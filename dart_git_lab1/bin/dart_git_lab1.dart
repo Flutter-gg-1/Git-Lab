100a96bcd8a1e57cc918dd968d1136c3c08b3f87
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 14:41:49 +0300

    Add 2 function

--- dart_git_lab1/bin/dart_git_lab1.dart
+++ dart_git_lab1/bin/dart_git_lab1.dart
@@ -1,7 +1,12 @@
 void main() {
-  print(myknowledge());
+  myknowledge1();
+  myknowledge1();
 }
 
-myknowledge() {
+myknowledge1() {
   print("Create a new git in CMD :\ngit init");
+}
+
+myknowledge2() {
+  print("Create a new branch in git by CMD :\ngit checkout -b NAME_OF_BRANCH");
 }
