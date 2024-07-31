f5e6f752dfcde221482c1a0d3c3580fd69ab9127
Ammarx9 <ammarxx919@gmail.com>
2024-07-31 14:43:08 +0300

    convelct info

--- bin/lab_soultion.dart
+++ bin/lab_soultion.dart
@@ -29,3 +29,9 @@ branch(){
 
   print("Branch is used to git copy of the code that will not afrct the main branch");
 }
+
+
+
+convlect(){
+  print("you may face some convelct when merge branch to the main bracnh you have to fix the code that cause the prmbolm ");
+}
