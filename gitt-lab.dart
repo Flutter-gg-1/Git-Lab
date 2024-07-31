82a3bd37441353d6e80da3effc89be2302cd4499
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-07-31 14:51:13 +0300

    my first try

---
+++ gitt-lab.dart
@@ -0,0 +1,30 @@
+
+import 'firstfunc.dart';
+
+void main() {
+  firstFunction();
+  secondFunction();
+  thierdFunction();
+  diffrentingit();
+  diffrentingithup();
+}
+
+
+secondFunction() {
+  print("github tarted at 2017");
+}
+
+thierdFunction() {
+  print(
+      "Git is a popular version control system. It was created by Linus Torvalds in 2005, and has been maintained by Junio Hamano since then.");
+}
+
+diffrentingit() {
+  print("Git is a software.");
+  print("Git is a command-line tool");
+}
+
+diffrentingithup() {
+  print("GitHub is a service.");
+  print("GitHub is a graphical user interface");
+}
