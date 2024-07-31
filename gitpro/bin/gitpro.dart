f8f234889df3edf5942155d46c071880cb977e14
muhannad <mohndalzaher@gmail.com>
2024-07-31 14:37:35 +0300

    man function added

---
+++ gitpro/bin/gitpro.dart
@@ -0,0 +1,5 @@
+import 'package:gitpro/gitpro.dart' as gitpro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gitpro.calculate()}!');
+}
---
+++ gitpro/test/gitpro_test.dart
@@ -0,0 +1,8 @@
+import 'package:gitpro/gitpro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
