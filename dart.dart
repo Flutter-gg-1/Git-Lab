218d113e6ec06cdf4d40873941678b52e799b92e
Nashruful <azooz.n40@hotmail.com>
2024-07-31 14:51:56 +0300

    git featured func

---
+++ dart.dart
@@ -0,0 +1,15 @@
+gitFeatures() {
+  print(
+      "Version Control: Track changes to files and manage multiple versions of your code.");
+  print(
+      "Branching: Create branches to work on different features or fixes without affecting the main codebase.");
+  print("Merging: Combine changes from different branches into one.");
+  print(
+      "Commit History: Record a history of changes, allowing you to revert to previous versions.");
+  print(
+      "Distributed: Each user has a full copy of the repository, enabling offline work and collaboration.");
+}
+
+void home() {
+  gitFeatures();
+}
