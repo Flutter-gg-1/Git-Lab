0995bb4c9d8f121258b861d3d03564ac16e29332
abdulaziz <aazizalamri5@gmail.com>
2024-07-31 14:34:50 +0300

    created dart project

---
+++ git-lab-app/bin/git_lab_app.dart
@@ -0,0 +1,5 @@
+import 'package:git_lab_app/git_lab_app.dart' as git_lab_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_lab_app.calculate()}!');
+}
---
+++ git-lab-app/test/git_lab_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_lab_app/git_lab_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
