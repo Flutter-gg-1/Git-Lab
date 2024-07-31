d6d56f751230f0771107f23808159b780940b04b
turki-aloufi <turkialoufi44@gmail.com>
2024-07-31 14:45:58 +0300

    sum function

--- my_quiz_app/bin/my_quiz_app.dart
+++ my_quiz_app/bin/my_quiz_app.dart
@@ -1,8 +1,12 @@
 void main(List<String> arguments) {
   printHello("turki");
+  print(sum(2,5));
 }
 
 printHello(String name) {
   print("hello $name");
 }
 
+sum(int num1, int num2) {
+  return num1 + num2;
+}
