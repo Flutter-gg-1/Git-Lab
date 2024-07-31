63067dd1d21c0a7b61fd57649b74ec4cf0bc5018
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 14:43:05 +0300

    Add 3 function

--- dart_git_lab1/bin/dart_git_lab1.dart
+++ dart_git_lab1/bin/dart_git_lab1.dart
@@ -1,6 +1,7 @@
 void main() {
   myknowledge1();
   myknowledge2();
+  myknowledge3();
 }
 
 myknowledge1() {
@@ -9,4 +10,8 @@ myknowledge1() {
 
 myknowledge2() {
   print("Create a new branch in git by CMD :\ngit checkout -b NAME_OF_BRANCH");
+}
+
+myknowledge3() {
+  print("How to show your branch in git by CMD? :\ngit branch");
 }
