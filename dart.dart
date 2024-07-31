cc09796f15a607f78bd82810d51887f8f01ddf4e
Nashruful <azooz.n40@hotmail.com>
2024-07-31 14:53:11 +0300

    gitHubFeatures func added

--- dart.dart
+++ dart.dart
@@ -9,7 +9,19 @@ gitFeatures() {
   print(
       "Distributed: Each user has a full copy of the repository, enabling offline work and collaboration.");
 }
+gitHubFeatures() {
+  print(
+      "Remote Repositories: Host your Git repositories online for easy collaboration and access.");
+  print(
+      "Pull Requests: Review, discuss, and merge changes from different branches or forks.");
+  print("Issues: Track bugs, tasks, and feature requests in your projects.");
+  print(
+      "Actions: Automate workflows with CI/CD pipelines and other automation tasks.");
+  print(
+      "Code Review: Facilitate code review processes with comments and suggestions directly on pull requests.");
+}
 
 void home() {
   gitFeatures();
+  gitHubFeatures();
 }
