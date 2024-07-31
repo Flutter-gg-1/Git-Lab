fc7fa0538723f8e549ddc969e7a3528f389d1123
Ali_Altarouty <tarooti14dev@gmail.com>
2024-07-31 14:47:44 +0300

    Transfer all function to new file myfunction.dart

--- dart_git_lab1/bin/dart_git_lab1.dart
+++ dart_git_lab1/bin/dart_git_lab1.dart
@@ -1,28 +1,9 @@
+import 'myfunctions.dart';
+
 void main() {
   myknowledge1();
   myknowledge2();
   myknowledge3();
   myknowledge4();
   myknowledge5();
-}
-
-myknowledge1() {
-  print("Create a new git in CMD :\ngit init");
-}
-
-myknowledge2() {
-  print("Create a new branch in git by CMD :\ngit checkout -b NAME_OF_BRANCH");
-}
-
-myknowledge3() {
-  print("How to show your branch in git by CMD? :\ngit branch");
-}
-
-myknowledge4() {
-  print("How to add your files in git by CMD? :\n(git add .)");
-}
-
-myknowledge5() {
-  print(
-      "How to push files in github by CMD? :\ngit push -u origin NAME_OF_BRANCH");
 }
